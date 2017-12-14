87749cb7052032bb974b585c09ded53478d1fb74
Miguel Francisco Teixeira Cruz <2069617@student.uma.pt>
2017-12-14 19:59:36 +0000

    paragrafo 1

--- src/App.vue
+++ src/App.vue
@@ -63,6 +63,7 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <p>pxsdcvg</p>
 
 
   </div>
